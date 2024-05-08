cd344e4b6bfa9314e690e832e6aecea4353c9df1
JakubFr4czek <jakubfraczek@student.agh.edu.pl>
2024-05-08 11:27:51 +0200

    Finished lab7

--- lab7/client.c
+++ lab7/client.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <mqueue.h>
 #include <unistd.h>
+#include <string.h>
 
 #define QUEUE_NAME "/server_queue"
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 256
 
 int main(){
 
     // Possible types of messages,
     // INIT - contains IPC key
+    // ID - id of a client
     // DATA - contains any data
     typedef enum {
         INIT,
+        ID,
         DATA
     }Msg_type;
 
@@ -19,7 +22,8 @@ int main(){
     typedef struct{
         
         Msg_type msg_type;
-        char* buffer;
+        int id;
+        char buffer[BUFFER_SIZE];
 
     }Message;
 
@@ -31,9 +35,9 @@ int main(){
     };
 
     //Opening the server queue
-     mqd_t descriptor = mq_open(QUEUE_NAME, O_RDWR, S_IRUSR | S_IWUSR, &attr);
+     mqd_t server_descriptor = mq_open(QUEUE_NAME, O_RDWR, S_IRUSR | S_IWUSR, &attr);
 
-    if(descriptor == -1){
+    if(server_descriptor == -1){
         perror("Error while opening queue\n");
         return 1;
     }
@@ -41,33 +45,85 @@ int main(){
     char client_queue[BUFFER_SIZE];
     sprintf(client_queue, "/client_queue_%d", getpid());
 
+    // Opening client queue
+    mqd_t client_descriptor = mq_open(client_queue, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR, &attr);
+
+    // Constructing INIT message
     Message init_message;
+    init_message.id = -1; // Client has no id yet
     init_message.msg_type = INIT;
-    init_message.buffer = client_queue;
+    memcpy(init_message.buffer, client_queue, sizeof(client_queue)); // It needs to be copied manually, otherwise it results in segementation fault
 
-    //mqd_t client_descriptor = mq_open(client_queue, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR, &attr);
-
-    // Sending init to server
-    if(mq_send(descriptor, (char*)&init_message, sizeof(Message), 0) == -1){
+    // Sending init to the server
+    if(mq_send(server_descriptor, (char*)&init_message, sizeof(init_message), 0) == -1){
         perror("Error while sending init to server\n");
         return 1;
     }
 
-    Message receivedMessage;
+    // Receiving an id from the server
+    Message id_message;
 
-    // Waiting for data
-    while(1) {
+    ssize_t bytes_read = mq_receive(client_descriptor, (char*)&id_message, sizeof(id_message), 0);
 
-        ssize_t bytes_read = mq_receive(descriptor, (char*)&receivedMessage, sizeof(Message), 0);
+    if (bytes_read == -1) {
 
-        if (bytes_read == -1) {
-            perror("mq_receive\n");
-            continue;
+        perror("mq_receive\n");
+            
+    }
+
+    int clientID = id_message.id;
+
+    pid_t pid = fork();
+
+    while(1){
+
+        if(pid == 0){ // Receiving messages from the server
+
+            Message message;
+
+            ssize_t bytes_read = mq_receive(client_descriptor, (char*)&message, sizeof(message), 0);
+
+            if (bytes_read == -1) {
+
+                perror("mq_receive\n");
+                    
+            }
+
+            switch(message.msg_type){
+
+                case DATA:
+                    printf("Message from client %d: %s\n", message.id, message.buffer);
+                    break;
+                default:
+                    perror("Wrong message type!");
+                    break;
+
+            }
+
+            
+        }else{ // Sending messages to the server
+
+            char input[BUFFER_SIZE];
+            scanf("%256[^\n]%*c", input);
+
+            Message message;
+            message.msg_type = DATA;
+            message.id = clientID;
+            memcpy(message.buffer, input, sizeof(input));
+
+            if(mq_send(server_descriptor, (char*)&message, sizeof(message), 0) == -1){
+                perror("Error while sending init to server\n");
+                return 1;
+            }
+
         }
 
-        printf("Hooray!\n");
 
     }
+    
+    
+
+
 
 
 
--- lab7/server.c
+++ lab7/server.c
@@ -3,17 +3,23 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define QUEUE_NAME "/server_queue"
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 256
+#define MAX_CLIENTS 15
 
 int main() {
     
+    printf("Server starting...\n");
+
     // Possible types of messages,
     // INIT - contains IPC key
+    // ID - id of a client
     // DATA - contains any data
     typedef enum {
         INIT,
+        ID,
         DATA
     }Msg_type;
 
@@ -21,44 +27,96 @@ int main() {
     typedef struct{
         
         Msg_type msg_type;
-        char* buffer;
+        int id;
+        char buffer[BUFFER_SIZE];
 
     }Message;
 
+    int registered_clients = 0;
+    mqd_t client_queues[MAX_CLIENTS];
+
+    // Setting attributes of the queue
     struct mq_attr attr = {
         .mq_flags = 0,
         .mq_msgsize = sizeof(Message),
         .mq_maxmsg = 10
     };
 
-    mqd_t descriptor = mq_open(QUEUE_NAME, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR, &attr);
+    // Opening server queue
+    mqd_t server_descriptor = mq_open(QUEUE_NAME, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR, &attr);
 
-    if (descriptor == -1) {
+    if (server_descriptor == -1) {
         perror("mq_open\n");
         return 1;
     }
 
+    printf("Server started\n");
+
+    // Holds a received message from the client
     Message receivedMessage;
 
     while(1) {
+        
+        // Receiving a message from the clients
+        if(mq_receive(server_descriptor, (char*)&receivedMessage, sizeof(receivedMessage), 0) == -1){
 
-        ssize_t bytes_read = mq_receive(descriptor, (char*)&receivedMessage, sizeof(Message), 0);
-
-        if (bytes_read == -1) {
             perror("mq_receive\n");
             continue;
         }
 
         switch(receivedMessage.msg_type){
 
-            case INIT:
-                printf("INIT\n");
-                printf("%s\n",receivedMessage.buffer);
+            case INIT: //A new client wants to join the server
+
+                printf("A new client joined the server\n");
+                
+                // Registering a client
+                if(registered_clients < MAX_CLIENTS){
+                    
+                    // Adding client queue descriptior to the list
+                    client_queues[registered_clients] = mq_open(receivedMessage.buffer, O_RDWR, S_IRUSR | S_IWUSR, &attr);
+
+                    // Creating a message containing client's new id
+                    Message clientID;
+                    clientID.msg_type = ID;
+                    clientID.id = registered_clients;
+    
+                    // Sending a client their id
+                    if(mq_send(client_queues[registered_clients], (char*)&clientID, sizeof(clientID), 0) == -1){
+                        perror("Error while sending id to the client\n");
+                        return 1;
+                    }
+
+                    registered_clients += 1;
+
+                }else{
+
+                    perror("Maximum number of clients reached!");
+                    continue;
+
+                }
                 break;
-            case DATA:
-                printf("DATA\n");
+
+            case DATA: //A client sends data to the server
+
+                // Sending data to all clients, but client with recivedMessage.id
+                for(int i = 0; i < registered_clients; i += 1){
+
+                    if(i != receivedMessage.id){
+
+                        if(mq_send(client_queues[i], (char*)&receivedMessage, sizeof(receivedMessage), 0) == -1){
+                        perror("Error while sending message to a client\n");
+                        return 1;
+                        }
+
+                    }
+
+                }
+
                 break;
-            default:
+
+            default: // Wrong message type or no message type
+
                 perror("Wrong message type\n");
                 break;
 
@@ -66,12 +124,14 @@ int main() {
 
     }
 
-    if(mq_close(descriptor) == -1) {
+    // Closing server queue
+    if(mq_close(server_descriptor) == -1) {
         perror("mq_close\n");
         return 1;
     }
 
-    if(mq_unlink(QUEUE_NAME) == -1) {
+    // Destroying server queue
+    if(mq_unlink(QUEUE_NAME) == -1) { // NEEDS TO BE CALLED AFTER CHANGING PARAMETER OF THE QUEUE!
         perror("mq_unlink\n");
         return 1;
     }
